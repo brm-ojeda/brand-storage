58eecdc1a4199b0416544b788527b3496a38c42e
Cristian Cortés <cristian.cortes@brm.com.co>
2018-05-29 16:13:34 -0500

    removed unused componets

--- public_html/src/dev/public/guidelines/guidelines.component.ts
+++ public_html/src/dev/public/guidelines/guidelines.component.ts
@@ -5,48 +5,50 @@ import { list, items } from '../../services/list.animation';
 
 
 @Component({
-  templateUrl: './guidelines.component.html',
-  animations:[list(),items()]
-  
+	templateUrl: './guidelines.component.html',
+	animations:[list(),items()]
+	
 })
 export class GuideComponent implements OnInit {
 
 	guides:any;
 
-  constructor(
-  	@Inject(DOCUMENT) private document: any
-  	) { }
+	constructor(
+		@Inject(DOCUMENT) private document: any
+		) { }
 
-  ngOnInit() {
-  	this.guides = [
-  		{
-  			title: 'Tenía en su casa una ama que pasaba de los cuarenta',
-  			img: 'img.jpg',
-  			desc: 'En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor.',
-        link:'hola'
-  		},
-      {
-        title: 'Tenía en su casa una ama que pasaba de los cuarenta',
-        img: 'img.jpg',
-        desc: 'En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor.',
-        link:'hola'
-      },
-      {
-        title: 'Tenía en su casa una ama que pasaba de los cuarenta',
-        img: 'img.jpg',
-        desc: 'En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor.',
-        link:'hola'
-      },
-      {
-        title: 'Tenía en su casa una ama que pasaba de los cuarenta',
-        img: 'img.jpg',
-        desc: 'En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor.',
-        link:'hola'
-      }
+	ngOnInit() {
 
-  	]
+		//arrays
+		this.guides = [
+			{
+				title: 'Tenía en su casa una ama que pasaba de los cuarenta',
+				img: 'img.jpg',
+				desc: 'En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor.',
+				link:'hola'
+			},
+			{
+				title: 'Tenía en su casa una ama que pasaba de los cuarenta',
+				img: 'img.jpg',
+				desc: 'En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor.',
+				link:'hola'
+			},
+			{
+				title: 'Tenía en su casa una ama que pasaba de los cuarenta',
+				img: 'img.jpg',
+				desc: 'En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor.',
+				link:'hola'
+			},
+			{
+				title: 'Tenía en su casa una ama que pasaba de los cuarenta',
+				img: 'img.jpg',
+				desc: 'En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor.',
+				link:'hola'
+			}
 
-  }
+		]
+
+	}
 
 
 }
